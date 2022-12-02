4fb241d31d11fda74b1f2c52ce25620ceb568ae0
adam clark <mr.adam.clark@gmail.com>
2022-12-02 07:50:41 -0700

    remove some commented code that was refactored during solving.

--- 2022/rust/src/day02/input.rs
+++ 2022/rust/src/day02/input.rs
@@ -11,21 +11,3 @@ pub fn read() -> Input<'static> {
          .map(|g| g.split_once(" ").unwrap())
          .collect()
 }
-
-// fn parse_rps_game(v: &str) -> (RPS, RPS) {
-//     let game: (&str,&str) = v.split_once(" ").unwrap();
-//     let a: RPS = match game.0 { 
-//         "A" => RPS::Rock,
-//         "B" => RPS::Paper,
-//         "C" => RPS::Scissors,
-//         _ => todo!(),
-//      };
-//     let b: RPS = match game.1 {
-//         "X" => RPS::Rock,
-//         "Y" => RPS::Paper,
-//         "Z" => RPS::Scissors,
-//         _ => todo!(),
-//      };
-
-//     (a, b)
-// }
