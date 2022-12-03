a29c75865dc1c56f017f575329d79cf98964771d
adam clark <mr.adam.clark@gmail.com>
2022-12-03 00:13:51 -0700

    dolved day3p2

--- 2022/rust/src/day03/part_two.rs
+++ 2022/rust/src/day03/part_two.rs
@@ -1,10 +1,13 @@
 use crate::day03::{Input};
 
 pub fn solve(input: &Input) -> u32 {
-    0
+    input.chunks(3)
+        .map(|group| find_badge(group.into()))
+        .map(|badge| score_badge(badge))
+        .sum::<u32>()
 }
 
-fn score_chars(c: char) -> u32 {
+fn score_badge(c: char) -> u32 {
     match c { 
         'a'..='z' => (c as u32) - 96,
         'A'..='Z' => ((c as u32) - 65) + 27,
@@ -12,6 +15,18 @@ fn score_chars(c: char) -> u32 {
     }
 }
 
-fn find_badge(s1: &str, s2: &str, s3: &str) -> char {
-    'a'
+fn find_badge(group: &[&str]) -> char {
+    let rucksacks: Vec<Vec<char>> = group.iter()
+                        .map(|g| g.chars().collect())
+                        .collect();
+    let mut badge: char = '-';
+    for item in &rucksacks[0] {
+        if rucksacks[1].contains(&item) && rucksacks[2].contains(&item) {
+            return *item;
+        }
+        else if rucksacks[2].contains(&item) {
+            badge = *item;
+        }
+    }
+    badge
  }
