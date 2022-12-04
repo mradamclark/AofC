f03bc28f4c9de64ae4c6f2a5b09584ef8d9f1b3b
adam clark <mr.adam.clark@gmail.com>
2022-12-03 22:44:21 -0700

    2022d04p2 solved in python

--- 2022/python/day04.py
+++ 2022/python/day04.py
@@ -14,6 +14,18 @@ for asg in assignments:
         fully_contained += 1
         
 print(fully_contained)
+
+disjoint = 0
+for asg in assignments:
+    g = asg.split(",")
+
+    elfA = set(range(int(g[0].split("-")[0]), int(g[0].split("-")[1])+1))
+    elfB = set(range(int(g[1].split("-")[0]), int(g[1].split("-")[1])+1))
+    
+    if elfA.isdisjoint(elfB):
+        disjoint += 1
+
+print(len(assignments) - disjoint)
     
     
     
