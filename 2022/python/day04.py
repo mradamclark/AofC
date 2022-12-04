c2523e8bbef1ec53c7def0968be2a9c6ef85c0bd
adam clark <mr.adam.clark@gmail.com>
2022-12-03 22:41:02 -0700

    2022d04p1 in python

---
+++ 2022/python/day04.py
@@ -0,0 +1,19 @@
+
+assignments = []
+with open("../input/day04/input.txt") as f:
+    assignments = [x.strip() for x in f.readlines()]
+ 
+fully_contained = 0
+for asg in assignments:
+    g = asg.split(",")
+
+    elfA = set(range(int(g[0].split("-")[0]), int(g[0].split("-")[1])+1))
+    elfB = set(range(int(g[1].split("-")[0]), int(g[1].split("-")[1])+1))
+
+    if elfA.issubset(elfB) or elfB.issubset(elfA):
+        fully_contained += 1
+        
+print(fully_contained)
+    
+    
+    
