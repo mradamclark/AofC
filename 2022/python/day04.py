5667feb100635513dd0fca59fac5cbaf6abb8bab
adam clark <mr.adam.clark@gmail.com>
2022-12-03 22:46:51 -0700

    clean up code.

--- 2022/python/day04.py
+++ 2022/python/day04.py
@@ -4,28 +4,19 @@ with open("../input/day04/input.txt") as f:
     assignments = [x.strip() for x in f.readlines()]
  
 fully_contained = 0
-for asg in assignments:
-    g = asg.split(",")
-
-    elfA = set(range(int(g[0].split("-")[0]), int(g[0].split("-")[1])+1))
-    elfB = set(range(int(g[1].split("-")[0]), int(g[1].split("-")[1])+1))
-
-    if elfA.issubset(elfB) or elfB.issubset(elfA):
-        fully_contained += 1
-        
-print(fully_contained)
-
 disjoint = 0
 for asg in assignments:
     g = asg.split(",")
 
     elfA = set(range(int(g[0].split("-")[0]), int(g[0].split("-")[1])+1))
     elfB = set(range(int(g[1].split("-")[0]), int(g[1].split("-")[1])+1))
-    
+
     if elfA.isdisjoint(elfB):
         disjoint += 1
-
-print(len(assignments) - disjoint)
-    
-    
+        
+    if elfA.issubset(elfB) or elfB.issubset(elfA):
+        fully_contained += 1
+        
+print("AofC 2022 d04 p1: " + str(fully_contained))
+print("AofC 2022 d04 p2: " + str(len(assignments) - disjoint))    
     
