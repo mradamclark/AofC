26ea7f3c575119842cbf443bdcd71e479c98c5dd
adam clark <mr.adam.clark@gmail.com>
2022-12-11 14:00:31 -0700

    solutions for day 11, need help to find a magic number

--- 2022/rust/day-11/src/lib.rs
+++ 2022/rust/day-11/src/lib.rs
@@ -1,29 +1,177 @@
+use std::{collections::VecDeque, fmt::Display};
+
 pub mod handheld;
 pub mod parser;
 
+#[derive(Debug, Copy, Clone)]
+pub enum Value {
+    Curr,
+    Old,
+    Num(u64),
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Operation {
+    Add((Value, Value)),
+    Mult((Value, Value)),
+    Div((Value, Value)),
+}
+
+#[derive(Clone)]
+pub struct Monkey {
+    id: u32,
+    items: VecDeque<u64>,
+    operation: Operation,
+    test: Operation,
+    true_dest: u32,
+    false_dest: u32,
+    number_items_inspected: u64,
+}
+impl Display for Monkey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Monkey {} - inspections:{}, items:{:?}",
+            self.id, self.number_items_inspected, self.items
+        )
+    }
+}
+impl Monkey {
+    pub fn handle_next(&mut self, worry_level_divisor: u64, magic_number: u64) -> (u64, usize) {
+        let item = self.items.pop_front().unwrap();
+        self.number_items_inspected += 1;
+        let worry = match &self.operation {
+            Operation::Add((a, b)) => {
+                let num_a = match a {
+                    Value::Old => item,
+                    Value::Num(n) => *n,
+                    Value::Curr => item,
+                };
+                let num_b = match b {
+                    Value::Old => item,
+                    Value::Num(n) => *n,
+                    Value::Curr => item,
+                };
+                (num_a + num_b) % magic_number
+            }
+            Operation::Mult((a, b)) => {
+                let num_a = match a {
+                    Value::Old => item,
+                    Value::Num(n) => *n,
+                    Value::Curr => item,
+                };
+                let num_b = match b {
+                    Value::Old => item,
+                    Value::Num(n) => *n,
+                    Value::Curr => item,
+                };
+                (num_a * num_b) % magic_number
+            }
+            _ => panic!("Not Supported"),
+        } / worry_level_divisor;
+
+        let send_to = match &self.test {
+            Operation::Div((a, b)) => {
+                let num_a = match a {
+                    Value::Curr => worry,
+                    Value::Num(n) => *n,
+                    Value::Old => item,
+                };
+                let num_b = match b {
+                    Value::Curr => worry,
+                    Value::Num(n) => *n,
+                    Value::Old => item,
+                };
+
+                if num_a % num_b == 0 {
+                    self.true_dest as usize
+                } else {
+                    self.false_dest as usize
+                }
+            }
+            _ => panic!("Arg, Don't know how to test"),
+        };
+
+        (worry, send_to)
+    }
+}
+
 pub fn process_part1(input: &str) -> String {
-    "todo".to_string()
+    let (_, mut monkeys) = parser::parse(input).unwrap();
+    let magic_n = get_magic_number(&monkeys);
+    for _round in 0..20 {
+        for mindex in 0..monkeys.len() {
+            for _ in 0..monkeys[mindex].items.len() {
+                let monkey = monkeys.get_mut(mindex).unwrap();
+                let (item, new_monkey_index) = monkey.handle_next(3, magic_n);
+                monkeys[new_monkey_index].items.push_back(item);
+            }
+        }
+    }
+
+    monkeys.sort_by_key(|monkey| monkey.number_items_inspected);
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|monkey| monkey.number_items_inspected)
+        .product::<u64>()
+        .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    "todo".to_string()
+    let (_, mut monkeys) = parser::parse(input).unwrap();
+
+    let magic_n = get_magic_number(&monkeys);
+
+    for _round in 0..10000 {
+        for mindex in 0..monkeys.len() {
+            for _ in 0..monkeys[mindex].items.len() {
+                let monkey = monkeys.get_mut(mindex).unwrap();
+                let (item, new_monkey_index) = monkey.handle_next(1, magic_n);
+                monkeys[new_monkey_index].items.push_back(item);
+            }
+        }
+    }
+
+    monkeys.sort_by_key(|monkey| monkey.number_items_inspected);
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|monkey| monkey.number_items_inspected)
+        .product::<u64>()
+        .to_string()
+}
+
+fn get_magic_number(monkeys: &Vec<Monkey>) -> u64 {
+    let magic_n = monkeys
+        .iter()
+        .map(|m| m.test)
+        .map(|o| match o {
+            Operation::Div((_, b)) => match b {
+                Value::Num(n) => n,
+                _ => panic!("oops"),
+            },
+            _ => panic!("oops 2"),
+        })
+        .product::<u64>();
+    magic_n
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
 
+    const INPUT: &str = include_str!("../data/test.txt");
     #[test]
     fn part_1_works() {
-        let input = fs::read_to_string("data/test.txt").unwrap();
-        let result = crate::process_part1(&input);
-        assert_eq!(result, "someanswer");
+        let result = crate::process_part1(INPUT);
+        assert_eq!(result, "10605");
     }
 
     #[test]
-    fn part2_works() {
-        let input = fs::read_to_string("./data/test.txt").unwrap();
-        let result = crate::process_part2(&input);
-        assert_eq!(result, "someanswer");
+    fn part_2_works() {
+        let result = crate::process_part2(INPUT);
+        assert_eq!(result, "2713310158");
     }
 }
--- 2022/rust/day-11/src/parser/mod.rs
+++ 2022/rust/day-11/src/parser/mod.rs
@@ -1,21 +1,90 @@
-use crate::handheld::Instruction;
+use std::collections::VecDeque;
+
+use crate::{Monkey, Operation, Value};
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{self, newline},
-    multi::separated_list1,
-    sequence::preceded,
+    character::complete::{self, multispace1, newline},
+    multi::{separated_list0, separated_list1},
+    sequence::{delimited, preceded, separated_pair},
     *,
 };
 
-pub fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
-    let (input, instructions) = separated_list1(
-        newline,
-        alt((
-            tag("noop").map(|_| Instruction::NoOp),
-            preceded(tag("addx "), complete::i32).map(|n| Instruction::AddX(n)),
-        )),
-    )(input)?;
+// Monkey 0:
+//   Starting items: 79, 98
+//   Operation: new = old * 19
+//   Test: divisible by 23
+//     If true: throw to monkey 2
+//     If false: throw to monkey 3
 
-    Ok((input, instructions))
+fn value(s: &str) -> IResult<&str, Value> {
+    alt((
+        tag("old").map(|_| Value::Old),
+        complete::u64.map(|n| Value::Num(n)),
+    ))(s)
+}
+
+fn operation(s: &str) -> IResult<&str, Operation> {
+    let (s, _) = tag("Operation: new = ")(s)?;
+    let (s, value_1) = value(s)?;
+    let (s, operator) = delimited(multispace1, alt((tag("*"), tag("+"))), multispace1)(s)?;
+    let (s, value_2) = value(s)?;
+
+    let result = match operator {
+        "+" => Operation::Add((value_1, value_2)),
+        "*" => Operation::Mult((value_1, value_2)),
+        _ => panic!("Arg, unknown opcode"),
+    };
+
+    Ok((s, result))
+}
+
+fn test(s: &str) -> IResult<&str, Operation> {
+    let (s, divisor) = preceded(tag("Test: divisible by "), complete::u64)(s)?;
+    let test_operation = Operation::Div((Value::Curr, Value::Num(divisor)));
+    Ok((s, test_operation))
+}
+
+fn starting_items(s: &str) -> IResult<&str, VecDeque<u64>> {
+    let (s, _) = tag("Starting items: ")(s)?;
+    let (s, items) = separated_list0(tag(", "), complete::u64)(s)?;
+    Ok((s, VecDeque::from(items)))
+}
+
+// Monkey 0:
+//   Starting items: 79, 98
+//   Operation: new = old * 19
+//   Test: divisible by 23
+//     If true: throw to monkey 2
+//     If false: throw to monkey 3
+
+fn monkey(s: &str) -> IResult<&str, Monkey> {
+    let (s, id) = delimited(tag("Monkey "), complete::u32, tag(":"))(s)?;
+    let (s, _) = multispace1(s)?;
+    let (s, items) = starting_items(s)?;
+    let (s, _) = multispace1(s)?;
+    let (s, operation) = operation(s)?;
+    let (s, _) = multispace1(s)?;
+    let (s, test) = test(s)?;
+    let (s, _) = multispace1(s)?;
+    let (s, true_dest) = preceded(tag("If true: throw to monkey "), complete::u32)(s)?;
+    let (s, _) = multispace1(s)?;
+    let (s, false_dest) = preceded(tag("If false: throw to monkey "), complete::u32)(s)?;
+
+    Ok((
+        s,
+        Monkey {
+            id,
+            items,
+            operation,
+            test,
+            true_dest,
+            false_dest,
+            number_items_inspected: 0,
+        },
+    ))
+}
+
+pub fn parse(s: &str) -> IResult<&str, Vec<Monkey>> {
+    separated_list1(tag("\n\n"), monkey)(s)
 }
